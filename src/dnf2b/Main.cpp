5e6379213c5a2050da6b272aeadc8ae1c78c3c36
Olivia <zoe.i2k1@gmail.com>
2022-08-12 20:17:25 +0200

    Add config installation, because why not

--- src/dnf2b/Main.cpp
+++ src/dnf2b/Main.cpp
@@ -10,5 +10,12 @@ int main(int argc, const char* argv[]) {
         std::cerr << "Must be root to run the program." << std::endl;
         return -1;
     }
-    dnf2b::CLI::parse(argc, argv);
+    try {
+        dnf2b::CLI::parse(argc, argv);
+    } catch (const int& x) {
+        // I'm abusing int throwing as an overcomplicated way to return the value,
+        // because why not? It works, and it's the only way to do this in a system where
+        // whether the errors warrant a restart is up to the specific error.
+        return x;
+    }
 }
--- src/dnf2b/core/Context.cpp
+++ src/dnf2b/core/Context.cpp
@@ -10,15 +10,13 @@ Context::Context(const std::vector<std::string>& arguments) :  arguments(argumen
     std::ifstream stream("/etc/dnf2b/config.local.json");
     if (!stream) {
         std::cerr << "Failed to find config.local.json" << std::endl;
-        // TODO: make a new exception type, catch in main(), return 255 to avoid continued reboots
-        throw std::runtime_error("Config load failed");
+        throw 255;
     }
     
     nlohmann::json custom;
     stream >> custom;
 
     config.update(custom, true);
-
 }
 
 void Context::checkHealth() {
