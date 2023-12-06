df94066dd1c781b74d2e3e9085b098cc8e0091ff
Olivia <zoe.i2k1@gmail.com>
2023-12-06 02:37:27 +0100

    Oversight: make install needs to be sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ git clone https://github.com/LunarWatcher/dnf2b
 
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX /opt/dnf2b
-make -j $(nproc) install
+sudo make -j $(nproc) install
