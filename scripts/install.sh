43b18fac8f3a07597a7b55d47f57edd1a5920ecd
Olivia <zoe.i2k1@gmail.com>
2023-12-06 02:42:27 +0100

    Typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/LunarWatcher/doesnotfail2ban dnf2b
 cd dnf2b
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX /opt/dnf2b
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/dnf2b
 sudo make -j $(nproc) install
