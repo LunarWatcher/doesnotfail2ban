f9b7d15fd69f88e148b23571749f00dd0556d374
Olivia <zoe.i2k1@gmail.com>
2023-12-06 02:39:41 +0100

    More dumb oversights

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-git clone https://github.com/LunarWatcher/dnf2b
-
+git clone https://github.com/LunarWatcher/doesnotfail2ban dnf2b
+cd dnf2b
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX /opt/dnf2b
 sudo make -j $(nproc) install
