d8be56cdfddeca8a5ed8ed8fe10e5b873c13fc22
Olivia <zoe.i2k1@gmail.com>
2023-12-09 03:06:41 +0100

    Add a delete-lockfile command

--- src/dnf2b/ui/CLI.cpp
+++ src/dnf2b/ui/CLI.cpp
@@ -7,6 +7,7 @@
 #include "stc/FileLock.hpp"
 #include "fmt/format.h"
 
+#include <thread>
 #include <unistd.h>
 
 namespace dnf2b {
@@ -39,7 +40,8 @@ int CLI::parse(int argc, const char* argv[]) {
         std::cout 
             << format("help", "Shows this helpful message")
             << format("health", "Runs a health check on the server")
-            << format("daemon", "Starts the dnf2b daemon");
+            << format("daemon", "Starts the dnf2b daemon")
+            << format("delete-lockfile", "Deletes the daemon lockfile. DO NOT RUN unless there's no daemon already running. This can and will break stuff.");
         std::cout << "Manual management:" << std::endl;
         std::cout
             << format("ban", "Manually ban one or more IPs")
@@ -62,7 +64,7 @@ int CLI::parse(int argc, const char* argv[]) {
         try {
             lock = std::make_shared<stc::FileLock>("/var/run/lock/dnf2b.daemon.lock");
         } catch (stc::FileLock::Errors e) {
-            spdlog::error("Failed to acquire daemon lock. Is dnf2b running already?");
+            spdlog::error("Failed to acquire daemon lock. Is dnf2b running already? If this is a mistake, run dnf2b delete-lockfile");
             return -2;
         } 
         
@@ -71,6 +73,14 @@ int CLI::parse(int argc, const char* argv[]) {
         // TODO
     } else if (command == "unban") {
 
+    } else if (command == "delete-lockfile") {
+        std::cout << "WARNING: Running this command while the daemon is running can and will result in bad stuff happening. "
+            << "Before running, make sure dnf2b is, in fact, not running. You can do this by running `ps aux | grep dnf2b`"
+            << "\n"
+            << "If you're in doubt, press Ctrl-C immediately. Otherwise, the lockfile will be deleted in 15 seconds."
+            << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(15));
+        std::filesystem::remove("/var/run/lock/dnf2b.daemon.lock");
     }
 
     return 0;
